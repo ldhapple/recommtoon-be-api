f21bd54c2181452239deeb8e37ce4e31074d22d3
leedohyun <lucky77795@naver.com>
2024-02-26 21:49:17 +0900

    Initial commit

---
+++ src/main/java/com/recommtoon/recommtoonapi/RecommtoonApplication.java
@@ -0,0 +1,13 @@
+package com.recommtoon.recommtoonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommtoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommtoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recommtoon/recommtoonapi/RecommtoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommtoon.recommtoonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommtoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
