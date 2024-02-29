97329e68de7e75ec557698d0e2729a439ae41d3c
leedohyun <lucky77795@naver.com>
2024-02-29 15:10:17 +0900

    feat: MbtiWebtoon Entity 추가.
    Mbti별 추천 웹툰을 저장하기 위한 엔티티.

---
+++ src/main/java/com/recommtoon/recommtoonapi/mbtitoon/entity/MbtiWebtoon.java
@@ -0,0 +1,25 @@
+package com.recommtoon.recommtoonapi.mbtitoon.entity;
+
+import com.recommtoon.recommtoonapi.base.entity.BaseEntity;
+import com.recommtoon.recommtoonapi.mbti.entity.Mbti;
+import com.recommtoon.recommtoonapi.webtoon.entity.Webtoon;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class MbtiWebtoon extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "mbti_id")
+    private Mbti mbti;
+
+    @ManyToOne
+    @JoinColumn(name = "webtoon_id")
+    private Webtoon webtoon;
+}
