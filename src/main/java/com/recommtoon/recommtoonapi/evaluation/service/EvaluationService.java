97a9e5d6945d11e7a635879aeb4503593f1cedd9
leedohyun <lucky77795@naver.com>
2024-03-10 21:09:05 +0900

    feat: Evaluation, Webtoon Service 구현.
    평가 데이터의 저장(update)과, 평가를 위한 Webtoon들의 데이터를 필터링해 가져오는 로직이 담긴 서비스.

---
+++ src/main/java/com/recommtoon/recommtoonapi/evaluation/service/EvaluationService.java
@@ -0,0 +1,43 @@
+package com.recommtoon.recommtoonapi.evaluation.service;
+
+import com.recommtoon.recommtoonapi.account.entity.Account;
+import com.recommtoon.recommtoonapi.account.repository.AccountRepository;
+import com.recommtoon.recommtoonapi.evaluation.entity.Evaluation;
+import com.recommtoon.recommtoonapi.evaluation.repository.EvaluationRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EvaluationService {
+
+    private final EvaluationRepository evaluationRepository;
+    private final AccountRepository accountRepository;
+
+    public Evaluation saveEvaluation(Evaluation evaluation) {
+        Long accountId = evaluation.getAccount().getId();
+        Long webtoonId = evaluation.getWebtoon().getId();
+
+        Optional<Evaluation> checkExistingEvaluation = evaluationRepository.findByAccountIdAndWebtoonId(accountId,
+                webtoonId);
+
+        if (checkExistingEvaluation.isPresent()) {
+
+            Evaluation existedEvaluation = checkExistingEvaluation.get();
+            existedEvaluation.updateRating(evaluation.getRating());
+
+            return existedEvaluation;
+        }
+
+        return evaluationRepository.save(evaluation);
+    }
+
+    public Long getEvaluatedCount(String username) {
+        Account loginAccount = accountRepository.findByUsername(username);
+
+        return evaluationRepository.countByAccountId(loginAccount.getId());
+    }
+}
---
+++ src/main/java/com/recommtoon/recommtoonapi/webtoon/service/WebtoonService.java
@@ -0,0 +1,42 @@
+package com.recommtoon.recommtoonapi.webtoon.service;
+
+import com.recommtoon.recommtoonapi.account.entity.Account;
+import com.recommtoon.recommtoonapi.account.repository.AccountRepository;
+import com.recommtoon.recommtoonapi.evaluation.repository.EvaluationRepository;
+import com.recommtoon.recommtoonapi.webtoon.dto.RatingWebtoonDto;
+import com.recommtoon.recommtoonapi.webtoon.entity.Webtoon;
+import com.recommtoon.recommtoonapi.webtoon.repository.CustomWebtoonRepository;
+import com.recommtoon.recommtoonapi.webtoon.repository.WebtoonRepository;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class WebtoonService {
+
+    private final WebtoonRepository webtoonRepository;
+    private final AccountRepository accountRepository;
+
+    public Page<RatingWebtoonDto> getNoEvaluateCards(String loginUsername, int page, int size) {
+        Account loginUser = accountRepository.findByUsername(loginUsername);
+        Pageable pageable = PageRequest.of(page, size, Sort.unsorted());
+
+        return webtoonRepository.findWebtoonsNotEvaluatedAndShuffled(loginUser.getId(), pageable);
+    }
+
+    public Webtoon findById(Long id) {
+        return webtoonRepository.findById(id).stream()
+                .findFirst()
+                .orElseThrow();
+    }
+}
