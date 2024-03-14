4b4253006cae1b7497eebc892fd98df0ab9a27c4
leedohyun <lucky77795@naver.com>
2024-03-15 04:36:43 +0900

    feat: 추천 시스템 구현에 필요한 상수들을 관리하는 Enum 클래스 구현

---
+++ src/main/java/com/recommtoon/recommtoonapi/recommendation/util/RecommendationConst.java
@@ -0,0 +1,16 @@
+package com.recommtoon.recommtoonapi.recommendation.util;
+
+import lombok.Getter;
+
+@Getter
+public enum RecommendationConst {
+    MINIMUM_EAVLUATION_COUNT(10),
+    RECOMMENDATION_COUNT(24),
+    HIGH_RATING_THRESHOLD(1);
+
+    private final int value;
+
+    RecommendationConst(int value) {
+        this.value = value;
+    }
+}
