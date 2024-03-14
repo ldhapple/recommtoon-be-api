cdb6dd04ad952fbae2515fac6488dd8d23036ba1
leedohyun <lucky77795@naver.com>
2024-03-14 10:02:18 +0900

    feat: 필요 Repository 메서드 구현
    선호 장르에 따른 랜덤한 웹툰을 조회하는 메서드

--- src/main/java/com/recommtoon/recommtoonapi/evaluation/repository/EvaluationRepository.java
+++ src/main/java/com/recommtoon/recommtoonapi/evaluation/repository/EvaluationRepository.java
@@ -12,4 +12,6 @@ public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
     Long countByAccountId(Long id);
 
     Optional<Evaluation> findByAccountIdAndWebtoonId(Long accountId, Long webtoonId);
+
+    List<Evaluation> findByAccountId(Long accountId);
 }
--- src/main/java/com/recommtoon/recommtoonapi/webtoon/repository/WebtoonRepository.java
+++ src/main/java/com/recommtoon/recommtoonapi/webtoon/repository/WebtoonRepository.java
@@ -1,7 +1,9 @@
 package com.recommtoon.recommtoonapi.webtoon.repository;
 
+import com.recommtoon.recommtoonapi.webtoon.entity.Genre;
 import com.recommtoon.recommtoonapi.webtoon.entity.Webtoon;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,7 @@ import java.util.List;
 public interface WebtoonRepository extends JpaRepository<Webtoon, Long>, CustomWebtoonRepository {
     @Query("select w from Webtoon w where w.titleId = :titleId")
     Optional<Webtoon> findByTitleId(@Param("titleId") String titleId);
+
+    @Query(value = "select * from webtoon w inner join webtoon_genre wg on w.id = wg.webtoon_id where wg.genre = ?1 order by RAND() LIMIT ?2", nativeQuery = true)
+    Set<Webtoon> findRandomWebtoonsByFavoriteGenreName(String genreName, int count);
 }
