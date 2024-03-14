a511c8021b28d9e934432e2053c145614e214ea5
leedohyun <lucky77795@naver.com>
2024-03-14 10:01:27 +0900

    chore: 필요 없는 엔티티 삭제 및 DTO 추가

---
+++ src/main/java/com/recommtoon/recommtoonapi/recommendation/dto/WebtoonRecommendationDto.java
@@ -0,0 +1,12 @@
+package com.recommtoon.recommtoonapi.recommendation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WebtoonRecommendationDto {
+
+    String titleId;
+    String imgSrc;
+}
