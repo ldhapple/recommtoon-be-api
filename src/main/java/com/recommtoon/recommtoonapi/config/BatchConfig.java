8901464b894cc350634acf9503cec53473b14943
leedohyun <lucky77795@naver.com>
2024-03-17 09:06:58 +0900

    fix: temp

--- src/main/java/com/recommtoon/recommtoonapi/config/BatchConfig.java
+++ src/main/java/com/recommtoon/recommtoonapi/config/BatchConfig.java
@@ -1,6 +1,6 @@
 package com.recommtoon.recommtoonapi.config;
 
-import com.recommtoon.recommtoonapi.crawler.tasklet.WebtoonCrawlingTasklet;
+//import com.recommtoon.recommtoonapi.crawler.tasklet.WebtoonCrawlingTasklet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
